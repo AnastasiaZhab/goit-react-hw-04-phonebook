fe8494c63f7e79a7717d96a92cb4f15dc2d00bfa
Anastasia Zhaboiedova <tsyfrak@gmail.com>
2022-09-09 13:57:24 +0300

    refactoring ContactForm

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -1,57 +1,62 @@
-import { Component } from "react";
+import { useState} from "react";
 import PropTypes from 'prop-types';
 import { nanoid } from "nanoid";
 import s from './ContactForm.module.css';
 
+export default function ContactForm(onSubmit) {
+    const [name, setName] = useState('');
+    const [number, setNumber] = useState('');
 
-class ContactForm extends Component {
-    static propTypes = {
-        onSubmit: PropTypes.func.isRequired,
-    };
-
-    state = {
-        name: "",
-        number: ""
-    }
-
-    handleSubmit = e => {
+    const handleSubmit = e => {
         e.preventDefault();
 
-        const { name, number } = this.state;
-
-        this.props.onSubmit({ id: nanoid(), name, number });
-        this.setState({ name: "", number: "" })
+        const id = nanoid();
+        onSubmit(id, name, number);
+         setName('');
+         setNumber('');
     }
 
-    handleChange = e => {
+    const handleChange = e => {
         const { name, value } = e.currentTarget;
-        this.setState({ [name]: value });
+
+        switch (name) {
+            case 'name':
+                setName(value);
+                break;
+            
+            case 'number':
+                setNumber(value);
+                break;
+            
+            default:
+                return;
+        }
     }
-    
-     
-    render() {
-        return(
-            <form className={s.form} onSubmit={this.handleSubmit}>
-                <label className={s.label} htmlFor="">Name
+
+
+    return (
+        
+        <form className={s.form} onSubmit={handleSubmit}>
+            <label className={s.label} htmlFor="">Name
                 <input className={s.input}
                     type="text"
                         name="name"
-                        value={this.state.name}
+                        value={name}
                         pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
                         title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
                         required
-                        onChange={this.handleChange}
+                        onChange={handleChange}
                     />
-                </label>
+            </label>
                 <label className={s.label} htmlFor="">Number
                 <input className={s.input}
                         type="tel"
-                        value={this.state.number}
+                        value={number}
                     name="number"
                     pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
                     title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
                     required
-                    onChange={this.handleChange}
+                    onChange={handleChange}
 
                     />
                     </label>
@@ -59,8 +64,7 @@ class ContactForm extends Component {
             </form>
         )
     }
-};
 
-export default ContactForm;
-
-
+ContactForm.propTypes = {
+      onSubmit: PropTypes.func.isRequired,
+    }
